a4421f97310387864985f18f5605fdffa2e0f3da
Shantanu Gupta <99300527+Shantanugupta43@users.noreply.github.com>
2022-10-26 14:00:09 +0100

    new music and alignment change

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ var audio = function() {
     var sound = new THREE.Audio(listener);
     var loader = new THREE.AudioLoader();
 
-    loader.load('fab.mp3', (buffer)=> {
+    loader.load('Manifest.mp3', (buffer)=> {
 
         sound.setBuffer(buffer);
         sound.setVolume(1);
